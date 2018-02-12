742d60ca51a63d7ac6ff5f95e340964b92579260
Komahan Boopathy <komahan.cool@gmail.com>
2018-02-11 19:46:52 -0500

    turned on debug flag for compilation; provided separate assembly function for dirichlet BC

--- test/conjugate-gradient/test_conjugate.f90
+++ test/conjugate-gradient/test_conjugate.f90
@@ -1,47 +1,51 @@
-program test  
-  !call check_conjugate  
+!=====================================================================!
+! Run gradient-based descent algorithms to solve linear algebra
+!=====================================================================!
+
+program test
+
+  implicit none
+
+  call check_conjugate
+
 end program test
+
+!---------------------------------------------------------------------!
+! 
+!---------------------------------------------------------------------!
 
 subroutine check_conjugate
 
   use linear_algebra
 
   implicit none
+  
+  integer, parameter :: npts = 64
+  real(8), parameter :: max_tol = 1.0d-6
+  integer, parameter :: max_it = 100000
+  real(8) :: x(npts), xtmp(npts), b(npts), A(npts,npts), P(npts, npts)
+  integer :: iter, flag, i, j
+  real(8) :: tol
 
-  print *, "conjugate gradients"
-  problem4: block
+  call assemble_system_dirichlet(0.0d0, 1.0d0, npts, A, b, x, P)
+  xtmp = solve(A,b)
 
-    integer, parameter :: npts = 1000
-    real(8), parameter :: max_tol = 1.0d-3
-    integer, parameter :: max_it  = 100000
+  call assemble_system_dirichlet(0.0d0, 1.0d0, npts, A, b, x, P)
+  call dsd(A, b, max_it, max_tol, x, iter, tol, flag)
+  !call dcg(A, b, max_it, max_tol, x, iter, tol, flag)
+  !call dpcg(A, P,  b, max_it, max_tol, x, iter, tol, flag)
 
-    real(8) :: x(npts), xtmp(npts), b(npts), A(npts,npts), P(npts, npts)
-    integer :: iter, flag, i, j
-    real(8) :: tol, omega(20)
-    real(8) :: scale = 1.0d0
+  print *, 'sd', tol, iter 
 
-    !call assemble_system(0.0d0, 1.0d0, npts, A, b, x, P)
-    !xtmp = solve(A,b)
-
-    call assemble_system(0.0d0, 1.0d0, npts, A, b, x, P)
-
-    !call dcg(A, b, max_it, max_tol, x, iter, tol, flag)    
-    !call dsd(A, b, max_it, max_tol, x, iter, tol, flag)    
-    call dpcg(A, P,  b, max_it, max_tol, x, iter, tol, flag)
-
-    print *, 'sd', tol, iter 
-
-    open(11, file='checkcg.dat')
-    do i = 1, npts
-       write(11, *) dble(i-1)/dble(npts), x(i), xtmp(i)
-    end do
-    close(11)
-
-  end block problem4
+  open(11, file='checkcg.dat')
+  do i = 1, npts
+     write(11, *) dble(i-1)/dble(npts), x(i), xtmp(i)
+  end do
+  close(11)
 
 end subroutine check_conjugate
 
-subroutine assemble_system(a, b, npts, V, rhs, x, P)
+subroutine assemble_system_mixed(a, b, npts, V, rhs, x, P)
 
   implicit none
   
@@ -103,4 +107,74 @@ subroutine assemble_system(a, b, npts, V, rhs, x, P)
      end do
   end do
 
-end subroutine assemble_system
+end subroutine assemble_system_mixed
+
+!---------------------------------------------------------------------!
+! Assemble -U_xx = 2x - 0.5, U(0) = U(1)= 0, x in [0,1]
+!---------------------------------------------------------------------!
+
+subroutine assemble_system_dirichlet(a, b, npts, V, rhs, u, P)
+
+  implicit none
+  
+  real(8), intent(in)  :: a, b ! bounds of the domain
+  integer              :: npts ! number of interior points
+  real(8), intent(out) :: V(npts,npts) ! banded matrix
+  real(8), intent(out) :: rhs(npts)
+  real(8), intent(out) :: u(npts)
+  real(8), intent(out) :: P(npts,npts)
+  
+  real(8), parameter :: PI = 3.141592653589793d0
+  real(8)            :: h, alpha
+  integer            :: M, N
+  integer            :: i, j, k
+  
+  ! h = width / num_interior_pts + 1
+  h = (b-a)/dble(npts+1)
+  V = 0.0d0
+
+  ! Size of the linear system = unknowns (interior nodes)
+  M = npts ! nrows
+  N = npts ! ncols
+  
+  ! Set the inner block
+  rows: do i = 1, M
+     cols: do j = 1, N
+        if (j .eq. i-1) then
+           ! lower triangle
+           V(j,i) = -1.0d0
+        else if (j .eq. i+1) then           
+           ! upper triangle
+           V(j,i) = -1.0d0
+        else if (j .eq. i) then           
+           ! diagonal
+           V(j,i) = 2.0d0
+        else
+           ! skip
+        end if
+     end do cols
+  end do rows
+  
+  ! Assemble the RHS
+  do i = 1, M
+     rhs(i) = h*h*(2.0d0*dble(i)*h - 0.5d0)
+  end do
+  rhs(1) = rhs(1) + 1.0d0
+  rhs(M) = rhs(M) + 1.0d0
+
+  ! Initial solution profile use sin function as a first guess
+  do i = 1, M
+     u(i) =  sin(dble(i)*h*PI)
+     print *, dble(i)*h, u(i)
+  end do
+
+  ! Set a preconditioner
+  !P = inv(V)
+!!$  alpha = sqrt(2.0d0/dble(npts+1))
+!!$  do j = 1, npts
+!!$     do k = 1, npts
+!!$        P(k,j) = alpha*sin(PI*dble(j*k)/dble(npts+1))
+!!$     end do
+!!$  end do
+
+end subroutine assemble_system_dirichlet
