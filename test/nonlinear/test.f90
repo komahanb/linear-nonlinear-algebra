b7eba402cbcb712860de40da416c282fe424c224
Komahan Boopathy <komahan.cool@gmail.com>
2018-03-29 19:23:35 -0400

    setup folders for nonlinear solvers

---
+++ test/nonlinear/test.f90
@@ -0,0 +1,11 @@
+!=====================================================================!
+! Run nonlinear solution algorithms on Chandrasekar-H equation
+!=====================================================================!
+
+program test_chandra
+
+  implicit none
+
+  print *, "hello world"
+  
+end program test_chandra
