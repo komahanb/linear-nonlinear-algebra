6bdb360a5902a8d98963efc3d97d197d45de00e1
Komahan Boopathy <komahan.cool@gmail.com>
2018-03-30 16:08:23 -0400

    updated test functions to vector than scalar

--- test/nonlinear/test.f90
+++ test/nonlinear/test.f90
@@ -9,40 +9,56 @@ module test_problems
 contains
   
   ! f = 2x^2 -5
-  real(8) function f1(x)    
-    real(8), intent(in) :: x
+  pure function f1(x)    
+    real(8), intent(in) :: x(:)
+    real(8) :: f1(size(x))
     f1 = 2.0d0*x*x - 5.0d0    
   end function f1
 
   ! dfdx = 4x
-  real(8) function dfdx1(x)    
-    real(8), intent(in) :: x
-    dfdx1 = 4.0d0*x
+  pure function dfdx1(x)
+    real(8), intent(in) :: x(:)
+    real(8) :: dfdx1(size(x),size(x))
+    dfdx1(1,1) = 4.0d0*x(1)
   end function dfdx1
 
   ! f = sin(x) + x
-  real(8) function f2(x)    
-    real(8), intent(in) ::  x   
+  pure function f2(x)    
+    real(8), intent(in) :: x(:)
+    real(8) :: f2(size(x))
     f2 = sin(x) + x
   end function f2
 
   ! dfdx = cos(x) + 1
-  real(8) function dfdx2(x)    
-    real(8), intent(in) :: x
-    dfdx2 = cos(x) + 1.0d0
+  pure function dfdx2(x)
+    real(8), intent(in) :: x(:)
+    real(8) :: dfdx2(size(x),size(x))
+    dfdx2(1,1) = cos(x(1)) + 1.0d0
   end function dfdx2
 
   ! f = cos(x)
-  real(8) function f3(x)    
-    real(8), intent(in) ::  x  
+  pure function f3(x)    
+    real(8), intent(in) ::  x(:)
+    real(8) :: f3(size(x))
     f3 = cos(x)
   end function f3
 
   ! dfdx = sin(x)
-  real(8) function dfdx3(x)
-    real(8), intent(in) :: x
-    dfdx3 = sin(x)
+  pure function dfdx3(x)
+    real(8), intent(in) :: x(:)
+    real(8) :: dfdx3(size(x),size(x))
+    dfdx3(1,1) = sin(x(1))
   end function dfdx3
+  
+  function res(x)
+    real(8), intent(in) :: x(:)
+    real(8) :: res(size(x))
+  end function res
+  
+  function jac(x)
+    real(8), intent(in) :: x(:)
+    real(8) :: jac(size(x), size(x))
+  end function jac
 
 end module test_problems
 
@@ -52,7 +68,7 @@ end module test_problems
 
 program test_nonlinear
 
-  use test_problems, only : f1, f2, f3
+  use test_problems, only : f1, f2, f3, dfdx1, dfdx2, dfdx3
   ! use nonlinear, only : newton
 
   implicit none
